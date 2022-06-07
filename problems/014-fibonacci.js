c8fea096bb36f4fc50db97c752ddc04e6146e4fd
Dmitry Fomin <dimafomin02@gmail.com>
2022-06-07 17:46:52 +0300

    problem 14 done

--- problems/014-fibonacci.js
+++ problems/014-fibonacci.js
@@ -17,7 +17,8 @@
  * @returns {number}
  */
 function fibonacci(n) {
-    return undefined;
+    if (n == 0) return 0;
+    return n == 1 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = fibonacci;
